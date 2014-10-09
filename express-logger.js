af0f07ff87c77aa492a99cdadfed38a6cbc0c7d6
markb_ef <markb@elementalfoundry.com>
2014-10-09 14:17:11 -0700

    Refactor, move client time normalization into this module

--- express-logger.js
+++ express-logger.js
@@ -8,16 +8,26 @@ module.exports = {
 };
 
 function init(config) {
-  var writeLog = loggerS3.init(config);
-  logger.init(writeLog);
-  logger.writeLog = writeLog;
+  if (config.maxEntriesPerFile) logger.maxEntries(config.maxEntriesPerFile);
+  if (config.queueFlushInterval) logger.ttl(config.queueFlushInterval);
+  loggerS3.init(config);
+  logger.init(loggerS3.writeLog);
+  logger.onError(console.error);
   return logger;
 }
 
 function clientLogger(request, response, next) {
   var args = request.body || request.query;
-  if (args.log) logger.writeLog(args.log, next);
-  else next();
+  if (args.log) {
+    var now = Date.now();
+    var timeDiff = (args.log.date || now) - now;
+    var logs = args.log.logs || [];
+    logs.forEach(function(logEntry) {
+      logEntry.date = (logEntry.date || now) - timeDiff;
+    });
+    if (logs.length) logger.append(logs); 
+  }
+  next();
 }
 
 function requestLogger(request, response, next) {
